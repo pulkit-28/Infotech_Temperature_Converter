a4cf5f916a5266da3a347d597a1615257b915b39
Pulkit Jain <137687873+pulkit-28@users.noreply.github.com>
2023-10-21 21:28:26 +0530

    Add files via upload

---
+++ Temperature converter.py
@@ -0,0 +1,5 @@
+celsius =int(input ("enter temperature in celsius: "))
+fahrenheit = (celsius *(1.8))+32
+kelvin = (celsius + 273.15 )
+print("the converted value is",fahrenheit,"fahrenheit")
+print("the converted value is",kelvin,"kelvin")
